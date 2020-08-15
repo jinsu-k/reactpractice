f7a588d929e543b72d1f9be9a5e7183c2817df83
jinsu-l <wlstn8738@naver.com>
2020-08-16 08:04:43 +0900

    출력할 데이터 개수 늘리기

--- src/App.js
+++ src/App.js
@@ -2,21 +2,62 @@ import React from 'react';
 import Customer from './components/Customer'
 import './App.css';
 
-const customer = {
-  'name':'홍길동',
-  'birthday':'961121',
-  'gender':'남자',
-  'job':'대학생'
-}
+const customer = [
+  {
+    'id':1,
+    'image':'https://placeimg.com/64/64/1',
+    'name':'홍길동',
+    'birthday':'961121',
+    'gender':'남자',
+    'job':'대학생'
+  },
+  {
+    'id':1,
+    'image':'https://placeimg.com/64/64/2',
+    'name':'아무개',
+    'birthday':'960101',
+    'gender':'남자',
+    'job':'백수'
+  },
+  {
+    'id':1,
+    'image':'https://placeimg.com/64/64/3',
+    'name':'김말똥',
+    'birthday':'960202',
+    'gender':'남자',
+    'job':'집사'
+  },
+]
 
 function App() {
   return (
-    <Customer
-    name={customer.name}
-    birthday={customer.birthday}
-    gender={customer.gender}
-    job={customer.job}
-    />
+    <div>
+      <Customer
+        id={customer[0].id}
+        image={customer[0].image}
+        name={customer[0].name}
+        birthday={customer[0].birthday}
+        gender={customer[0].gender}
+        job={customer[0].job}
+      />
+      <Customer
+        id={customer[1].id}
+        image={customer[1].image}
+        name={customer[1].name}
+        birthday={customer[1].birthday}
+        gender={customer[1].gender}
+        job={customer[1].job}
+      />
+      <Customer
+        id={customer[2].id}
+        image={customer[2].image}
+        name={customer[2].name}
+        birthday={customer[2].birthday}
+        gender={customer[2].gender}
+        job={customer[2].job}
+      />
+    </div>
+    
   );
 }
 
